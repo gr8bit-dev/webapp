fc1eccad7b0fc4455c2d4694a129afdbaa48951a
Tanner Marshall <tanner.s.marshall@gmail.com>
2021-04-29 14:31:51 -0400

    Added eslint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  semi: true, // Use semicolons at the end of lines
+  trailingComma: 'all', // Adds trailing commas whenever possible
+  singleQuote: true, // Use single quotes for strings
+  printWidth: 120, // Width that prettier will wrap on
+  tabWidth: 4 // Number of spaces per tab
+};
