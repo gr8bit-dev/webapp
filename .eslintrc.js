beb55fa6ab7b1ebe38a466d0cf02d47b3fbc5224
Tanner Marshall <tanner.s.marshall@gmail.com>
2021-05-03 08:01:43 -0400

    Added client .eslintrc.js file and updated server .eslintrc.js file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,11 @@
 module.exports = {
     env: {
-        browser: true,
-        es2021: true,
+        browser: false,
         node: true,
     },
-    extends: ['plugin:react/recommended', 'airbnb', 'plugin:prettier/recommended'],
+    extends: ['airbnb', 'plugin:prettier/recommended'],
     parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: 'module',
-    },
-    plugins: ['react', '@typescript-eslint'],
+    parserOptions: {},
+    plugins: ['@typescript-eslint'],
     rules: {},
 };
